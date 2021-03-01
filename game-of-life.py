4e7933c634db54f7d2a4d9dea234ddc1f947e7bc
James Cooper <58943271+jcoopertech@users.noreply.github.com>
2021-03-01 00:43:15 +0000

    Endgame, breeding, populating at end of turn.

--- game-of-life.py
+++ game-of-life.py
@@ -1,13 +1,17 @@
 #!/usr/bin python
 
 import random
-
+import time
 # Class definitions
+
+class Endgame(Exception):
+    def __init__(self, e=None):
+        pass
 
 class Cell:
     def __init__(self, y, x):
         self.Log = []
-        if random.randint(0,5) == 1:
+        if random.randint(0,10) == 1:
             self.setAlive()
         else:
             self.setDead()
@@ -28,6 +32,9 @@ class Cell:
         elif self.alive == False:
             return("-")
 
+    def add_log(self, stringtoadd):
+        self.Log.append(str(stringtoadd))
+
     def printLog(self):
         for iter in range(len(self.Log)):
             print(f"{iter}:{self.Log[iter]}")
@@ -35,8 +42,8 @@ class Cell:
 
 class Board:
     def __init__(self):
-        self.xrange = 10
-        self.yrange = 10
+        self.xrange = 5
+        self.yrange = 5
         self.Board = []
 
     def BoardSetup(self):
@@ -48,6 +55,8 @@ class Board:
 
 
     def printBoard(self):
+        time.sleep(0.02)
+        print(chr(27) + "[2J")
         for x in range(self.xrange):
             if len(str(x)) == 1:
                 prepend = " "
@@ -64,15 +73,15 @@ class Board:
 
     def resolveNextDoors(self,Row,Col) -> list:
         NextDoors = []
-        NextDoorOpts = [(0,-1),(-1,0),(0,1),(1,1),(1,0), \
-        (-1,-1),(1,-1),(-1,1)]
+        NextDoorOpts = [(0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0), \
+        (1,-1)]
         for NextDoorOpt in NextDoorOpts:
             NDoorRow = Row+NextDoorOpt[0]
             NDoorCol = Col+NextDoorOpt[1]
             # Stack overflow
-            if NDoorRow >= self.yrange - 1:
+            if NDoorRow > self.yrange - 1:
                 NDoorRow = 0
-            if NDoorCol >= self.xrange - 1:
+            if NDoorCol > self.xrange - 1:
                 NDoorCol = 0
             # Stack underflow
             if NDoorCol < 0:
@@ -85,14 +94,32 @@ class Board:
 
     def DoMate(self, HostCell, PartnerCell):
         #Find empty next-door, and set it to "alive"
+        PotentialSpawnCells = []
+        TurnBabies = []
         for coord in self.resolveNextDoors(HostCell.Y, HostCell.X):
-            HostCell.printLog()
+            if self.Board[coord[0]][coord[1]].alive == False:
+                PotentialSpawnCells.append(coord)
+        if len(PotentialSpawnCells) == 0:
+            return []
+        else:
+            random_coord = random.choice(PotentialSpawnCells)
+    #        print((HostCell.Y, HostCell.X), PotentialSpawnCells)
+            NewSpawn = Cell(random_coord[0],random_coord[1])
+            NewSpawn.setAlive()
+            NewSpawn.add_log(f"New spawn, born of {HostCell.Y},{HostCell.X} and {PartnerCell.Y},{PartnerCell.X}.")
+            TurnBabies.append(NewSpawn)
+        return(TurnBabies)
+
+    def PopulateBoard(self,TurnBabies):
+        if len(TurnBabies) != 0:
+            for Baby in TurnBabies:
+                self.Board[Baby.Y][Baby.X] = Baby
 
 
     def advanceTurn(self):
         for Row in range(self.yrange):
             for Col in range(self.xrange):
-                mate = False
+                needToPopulate = False
                 # Do edge wraparound
                 # Check if a partner exists.
 
@@ -101,11 +128,15 @@ class Board:
                     NextDoors = self.resolveNextDoors(Row,Col)
                     for NextDoor in NextDoors:
                         if self.Board[NextDoor[0]][NextDoor[1]].alive == True:
-                            mate = True
-                            if mate == True:
-                                self.DoMate(self.Board[Row][Col], self.Board[NextDoor[0]][NextDoor[1]])
-#                                print(f"Y:{Row},X:{Col} could (w/ consent) mate with Y:{NDoorRow},X:{NDoorCol}.")
-                                break
+                            TurnBabies = self.DoMate(self.Board[Row][Col], self.Board[NextDoor[0]][NextDoor[1]])
+                            #print(f"Y:{Row},X:{Col} could (w/ consent) mate with Y:{NextDoor[0]},X:{NextDoor[1]}.")
+                            needToPopulate = True
+                            break
+        if needToPopulate == True:
+            self.PopulateBoard(TurnBabies)
+        else:
+            raise Endgame("Motherfucker, we're done here.")
+        self.printBoard()
 
 
 # Global functions definitions
@@ -113,7 +144,8 @@ def main():
     GameBoard = Board()
     GameBoard.BoardSetup()
     GameBoard.printBoard()
-    GameBoard.advanceTurn()
+    while True:
+        GameBoard.advanceTurn()
 
 # Runtime
 if __name__ == "__main__":
